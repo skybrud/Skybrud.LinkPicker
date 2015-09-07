8d260915be974334bcef1d755f1ff3f7fd45bd12
Anders Bjerner <abjerner@skybrud.dk>
2015-09-07 23:17:28 +0200

    Rolling out my new and shiny build setup

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+
+	var path = require('path');
+
+	// Load the package JSON file
+	var pkg = grunt.file.readJSON('package.json');
+
+	// get the root path of the project
+	var projectRoot = 'src/' + pkg.name + '/';
+
+	// Load information about the assembly
+	var assembly = grunt.file.readJSON(projectRoot + 'Properties/AssemblyInfo.json');
+
+	// Get the version of the package
+	var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
+
+	grunt.initConfig({
+		pkg: pkg,
+		clean: {
+			files: [
+				'releases/temp/**/*.*'
+			]
+		},
+		copy: {
+			binary: {
+				files: [
+					{
+						expand: true,
+						cwd: projectRoot + 'bin/',
+						src: [
+							pkg.name + '.dll',
+							pkg.name + '.xml'
+						],
+						dest: 'releases/temp/bin/'
+					}
+				]
+			},
+			resources: {
+				files: [
+					{
+						expand: true,
+						cwd: projectRoot + 'App_Plugins/' + pkg.name + '/',
+						src: ['**/*.*'],
+						dest: 'releases/temp/App_Plugins/' + pkg.name + '/'
+					}
+				]
+			}
+		},
+		nugetpack: {
+			dist: {
+				src: 'src/' + pkg.name + '/' + pkg.name + '.csproj',
+				dest: 'releases/nuget/'
+			}
+		},
+		zip: {
+			release: {
+				cwd: 'releases/temp/',
+				src: [
+					'releases/temp/**/*.*'
+				],
+				dest: 'releases/github/' + pkg.name + '.v' + version + '.zip'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-nuget');
+	grunt.loadNpmTasks('grunt-zip');
+
+	grunt.registerTask('dev', ['clean', 'copy', 'nugetpack', 'zip']);
+
+	grunt.registerTask('default', ['dev']);
+
+};
