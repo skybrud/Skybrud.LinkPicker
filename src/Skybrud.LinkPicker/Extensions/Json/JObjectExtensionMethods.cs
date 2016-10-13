cabe28a8725c87f794df7242bcef3451577150ae
Anders Bjerner <abjerner@skybrud.dk>
2016-10-13 23:35:10 +0200

    Added depenndency for Skybrud.Essentials
    Rather than having somewhat the same logic in multiple packages, they should now use our new "core" library.

--- src/Skybrud.LinkPicker/Extensions/Json/JObjectExtensionMethods.cs
+++ src/Skybrud.LinkPicker/Extensions/Json/JObjectExtensionMethods.cs
@@ -4,51 +4,61 @@ using Newtonsoft.Json.Linq;
 
 namespace Skybrud.LinkPicker.Extensions.Json {
 
+    [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
     public static class JObjectExtensionMethods {
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static JObject GetObject(this JObject obj, string propertyName) {
             if (obj == null) return null;
             return obj.GetValue(propertyName) as JObject;
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static T GetObject<T>(this JObject obj, string propertyName) {
             if (obj == null) return default(T);
             JObject child = obj.GetValue(propertyName) as JObject;
             return child == null ? default(T) : child.ToObject<T>();
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static T GetObject<T>(this JObject obj, string propertyName, Func<JObject, T> func) {
             return obj == null ? default(T) : func(obj.GetValue(propertyName) as JObject);
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static string GetString(this JObject obj, string propertyName) {
             if (obj == null) return null;
             JToken property = obj.GetValue(propertyName);
             return property == null ? null : property.Value<string>();
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static int GetInt32(this JObject obj, string propertyName) {
             if (obj == null) return 0;
             JToken property = obj.GetValue(propertyName);
             return property == null ? 0 : property.Value<int>();
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static double GetDouble(this JObject obj, string propertyName) {
             if (obj == null) return 0;
             JToken property = obj.GetValue(propertyName);
             return property == null ? 0 : property.Value<double>();
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static bool GetBoolean(this JObject obj, string propertyName) {
             if (obj == null) return false;
             JToken property = obj.GetValue(propertyName);
             return property != null && property.Value<bool>();
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static JArray GetArray(this JObject obj, string propertyName) {
             return obj == null ? null : obj.GetValue(propertyName) as JArray;
         }
 
+        [Obsolete("Use extension methods from the Skybrud.Essemtials package instead.")]
         public static T[] GetArray<T>(this JObject obj, string propertyName, Func<JObject, T> func) {
 
             if (obj == null) return null;
--- src/Skybrud.LinkPicker/LinkPickerItem.cs
+++ src/Skybrud.LinkPicker/LinkPickerItem.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Skybrud.LinkPicker.Extensions.Json;
+using Skybrud.Essentials.Json.Extensions;
 using Umbraco.Core.Models;
 using Umbraco.Web;
 
