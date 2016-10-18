4bfad6dd5aaf5d9103dbff83c0e51aecdfda75fa
Anders Bjerner <abjerner@skybrud.dk>
2016-10-18 14:45:51 +0200

    New release :D

--- src/Skybrud.LinkPicker/Properties/AssemblyInfo.cs
+++ src/Skybrud.LinkPicker/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyInformationalVersion("1.0.11")]
+[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyInformationalVersion("1.0.12")]
