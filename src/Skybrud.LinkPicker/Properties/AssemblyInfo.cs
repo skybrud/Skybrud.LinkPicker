bd5a5fb2c09e0d183bcc3659c54bf6ac9b2867de
Anders Bjerner <abjerner@skybrud.dk>
2018-01-18 12:35:09 +0100

    New release :D

--- src/Skybrud.LinkPicker/Properties/AssemblyInfo.cs
+++ src/Skybrud.LinkPicker/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.PropertyEditors")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyInformationalVersion("1.1.2")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyInformationalVersion("1.1.3")]
--- src/Skybrud.LinkPicker/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.LinkPicker/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.774.1")]
+[assembly: AssemblyFileVersion("0.0.1028.4")]
 
