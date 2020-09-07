5030ec7cea87ef1615ec6125425ec9de2c1802ce
Anders Bjerner <abjerner@skybrud.dk>
2020-09-07 23:49:05 +0200

    Added constructor overloads to the LinkPickerLink class (implementing #14)

--- src/Skybrud.LinkPicker/Models/LinkPickerLink.cs
+++ src/Skybrud.LinkPicker/Models/LinkPickerLink.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Extensions;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
 
 namespace Skybrud.LinkPicker.Models {
 
@@ -41,6 +44,66 @@ namespace Skybrud.LinkPicker.Models {
             Type = obj.GetEnum("type", LinkPickerType.Url);
             Target = obj.GetString("target");
             Anchor = obj.GetString("anchor");
+        }
+
+        /// <summary>
+        /// Initializes a new link from the specified parameters.
+        /// </summary>
+        /// <param name="id">The numeric ID of the content or media item of the link.</param>
+        /// <param name="udi">The UDI of the content or media item of the link.</param>
+        /// <param name="name">The name of the link.</param>
+        /// <param name="url">The URL of the link.</param>
+        /// <param name="type">The type of the link.</param>
+        /// <param name="target">The target value of the link.</param>
+        /// <param name="anchor">The anchor value of the link.</param>
+        public LinkPickerLink(int id, string udi, string name, string url, LinkPickerType type, string target, string anchor) {
+            Id = id;
+            Udi = udi;
+            Name = name;
+            Url = url;
+            Type = type;
+            Target = target;
+            Anchor = anchor;
+        }
+
+        /// <summary>
+        /// Initializes a new link the specified content or media <paramref name="item"/>.
+        /// </summary>
+        /// <param name="item">An instance of <see cref="IPublishedContent"/> representing a content or media item.</param>
+        /// <param name="name">The name of the link.</param>
+        /// <param name="target">The target value of the link.</param>
+        /// <param name="anchor">The anchor value of the link.</param>
+        public LinkPickerLink(IPublishedContent item, string name, string target, string anchor) {
+            
+            if (item == null) throw new ArgumentNullException(nameof(item));
+
+            switch (item.ItemType) {
+
+                case PublishedItemType.Content:
+                    Id = item.Id;
+                    Udi = new GuidUdi("document", item.Key).ToString();
+                    Name = name ?? item.Name;
+                    Url = item.Url;
+                    Type = LinkPickerType.Content;
+                    Target = target;
+                    Anchor = anchor;
+                    break;
+
+                case PublishedItemType.Media:
+                    Id = item.Id;
+                    Udi = new GuidUdi("media", item.Key).ToString();
+                    Name = name ?? item.Name;
+                    Url = item.Url;
+                    Type = LinkPickerType.Media;
+                    Target = target;
+                    Anchor = anchor;
+                    break;
+
+                default:
+                    throw new ArgumentException($"Published item type {item.ItemType} is not supported.", nameof(item));
+
+            }
+
         }
 
         #endregion
