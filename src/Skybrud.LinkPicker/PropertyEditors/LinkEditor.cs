62e6e9ae9da4edb02ed60d0034b5ab42873350d7
Anders Bjerner <abjerner@skybrud.dk>
2021-06-04 12:01:02 +0200

    LinkPropertyEditor -> LinkEditor

--- src/Skybrud.LinkPicker/PropertyEditors/LinkEditor.cs
+++ src/Skybrud.LinkPicker/PropertyEditors/LinkEditor.cs
@@ -4,9 +4,9 @@ using Umbraco.Core.PropertyEditors;
 namespace Skybrud.LinkPicker.PropertyEditors {
 
     [DataEditor("Skybrud.LinkPicker.Link", EditorType.PropertyValue, "Skybrud LinkPicker Link", "/App_Plugins/Skybrud.LinkPicker/Views/Editors/Link.html", ValueType = ValueTypes.Json, Group = "Skybrud.dk", Icon = "icon-link")]
-    public class LinkPropertyEditor : DataEditor {
+    public class LinkEditor : DataEditor {
 
-        public LinkPropertyEditor(ILogger logger) : base(logger) { }
+        public LinkEditor(ILogger logger) : base(logger) { }
 
         /// <inheritdoc />
         protected override IConfigurationEditor CreateConfigurationEditor() => new LinkConfigurationEditor();
