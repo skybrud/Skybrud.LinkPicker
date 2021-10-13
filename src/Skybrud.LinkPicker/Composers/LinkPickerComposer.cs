80ded01b81285e9c1271b652afdc7feb603c775d
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-10-13 12:55:19 +0200

    Added MediaTracking to the linkpicker

---
+++ src/Skybrud.LinkPicker/Composers/LinkPickerComposer.cs
@@ -0,0 +1,14 @@
+﻿using Skybrud.LinkPicker.Tracking;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Skybrud.LinkPicker.Composers
+{
+    public class LinkPickerComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.DataValueReferenceFactories().Append<LinkMediaTracking>();
+        }
+    }
+}
---
+++ src/Skybrud.LinkPicker/Models/Tracking/LinkData.cs
@@ -0,0 +1,9 @@
+﻿namespace Skybrud.LinkPicker.Models.Tracking
+{
+    public class LinkData
+    {
+        public bool Permanent { get; set; }
+        public Destination Destination { get; set; }
+    }
+
+}
